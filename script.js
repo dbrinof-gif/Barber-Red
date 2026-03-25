2d48b06f7f75918bac6de5ea6f0a67b1ba779747
dbrinof-gif <dbrinof@gmail.com>
2026-03-25 03:44:01 +0100

    Add WhatsApp message functionality to script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hello() {
     alert("مرحبا بك في موقعي!");
 }
+let time = document.getElementById("time").value;
+let phone = "213540942085"; // رقمك
+let message = "حجز جديد:%0Aالاسم: " + name + "%0Aالخدمة: " + service + "%0Aالوقت: " + time;
+
+window.open("https://wa.me/" + phone + "?text=" + message);
