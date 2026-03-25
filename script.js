06be43145b211768d1f84ccb54278701f6e2c221
dbrinof-gif <dbrinof@gmail.com>
2026-03-25 03:45:48 +0100

    Add success message after booking
    Display success message after opening WhatsApp link.

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ let phone = "213540942085"; // رقمك
 let message = "حجز جديد:%0Aالاسم: " + name + "%0Aالخدمة: " + service + "%0Aالوقت: " + time;
 
 window.open("https://wa.me/" + phone + "?text=" + message);
+document.getElementById("result").innerHTML = "تم الحجز بنجاح 🎉";
